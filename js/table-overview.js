177d1ff0d63fdb7929012a58d8f7de90e33f291a
Bram Verrando <dhapacifist@gmail.com>
2024-08-08 01:57:37 +0200

    fetching and filtering button

--- js/table-overview.js
+++ js/table-overview.js
@@ -1,22 +1,22 @@
-const game1 = {name: "Fifa23", type: "Football", rating: 7, Favourite: false}
+const games = [];
 
-const game2 = {name: "AOTennis 2", type: "Tennis", rating: 2, Favourite: true }
+const fetchGames = async () => {
+    const response = await fetch("http://localhost:3000/games");
+    const result = await response.json();
+    games.push(...result);
+};
 
-const game3 = {name: "Elden Ring", type: "Soulslike", rating: 4, Favourite: false}
+const searchByFetch = async (chars) => {
+    const response = await fetch(`http://localhost:3000/games?query=${chars}`);
+    const result = await response.json();
+    return result;
+};
 
-const game4 = {name: "Horizon Forbidden West", type: "Adventure", rating: 3.5, Favourite: false }
 
-const game5 = {name: "Pokemon legends: Arceus", type: "RPG", rating: 3, Favourite: true}
-
-const game6 = {name: "GTA V", type: "Open World", rating: 5, Favourite: true}
-
-const game7 = {name: "Gran Turismo", type: "Car", rating: 6, Favourite: true}
-
-const games = [game1, game2, game3, game4, game5, game6, game7]
 
 const toString = (game) => {
-    return `Name: ${game.name} - Type: ${game.type} - Rating: ${game.rating} - Favourite: ${game.Favourite}`
-}
+    return `Name: ${game.name} - Type: ${game.type} - Rating: ${game.rating} - Favourite: ${game.Favourite}`;
+};
 
 const statusDiv = document.createElement('div');
 statusDiv.id = 'status';
@@ -31,7 +31,6 @@ h2party.addEventListener("click", () => {
 });
 
 const statusHeader = document.createElement('h3');
-
 
 const renderGames = (games, filterFunction) => {
     const tableBody = document.querySelector("#my-games-table-body");
@@ -57,13 +56,23 @@ const renderGames = (games, filterFunction) => {
     });
 };
 
+const fetchAndRenderGames = async () => {
+    await fetchGames();
+    renderGames(games);
+};
+
+const searchByFetchAndRender = async (chars) => {
+    const filteredGames = await searchByFetch(chars);
+    renderGames(filteredGames);
+    document.querySelector('#table-caption').textContent = `Games with name containing '${chars}'`;
+};
 statusHeader.textContent = 'Status';
 
 statusDiv.appendChild(statusHeader);
 
 document.querySelector('main').appendChild(statusDiv);
 
-renderGames(games)
+fetchAndRenderGames();
 
 document.querySelector("#show-favourites").addEventListener("click", () => {
     renderGames(games, game => game.Favourite);
@@ -82,3 +91,7 @@ document.querySelector("#rating-field").addEventListener("input", (event) => {
     }
 });
 
+document.querySelector("#fetch-games").addEventListener("click", () => {
+    const searchQuery = document.querySelector("#search-field").value;
+    searchByFetchAndRender(searchQuery);
+});
