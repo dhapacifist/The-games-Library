364056a0162f280c1c8c4c8e3f0f61aa521ba313
Bram Verrando <dhapacifist@gmail.com>
2024-08-07 22:50:14 +0200

    adding elements to java edit (complete)

--- js/table-overview.js
+++ js/table-overview.js
@@ -7,8 +7,6 @@ const statusHeader = document.createElement('h3');
 
 statusHeader.textContent = 'Status';
 
-// Step 5: Append the h3 to the div
 statusDiv.appendChild(statusHeader);
 
-// Step 6: Append the div to the main element
 document.querySelector('main').appendChild(statusDiv);
