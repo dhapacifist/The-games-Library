5196db0be09b904a19301b75e6dc9a310c3482c0
Bram Verrando <dhapacifist@gmail.com>
2024-08-08 00:19:51 +0200

    click action each table exercise

--- js/table-overview.js
+++ js/table-overview.js
@@ -14,25 +14,47 @@ const game7 = {name: "Gran Turismo", type: "Car", rating: 6, Favourite: true}
 
 const games = [game1, game2, game3, game4, game5, game6, game7]
 
+const toString = (game) => {
+    return `Name: ${game.name} - Type: ${game.type} - Rating: ${game.rating} - Favourite: ${game.Favourite}`
+}
+
 const statusDiv = document.createElement('div');
 statusDiv.id = 'status';
 
+statusDiv.addEventListener("mouseover", () => statusDiv.setAttribute("style", "background-color: green;"));
+statusDiv.addEventListener("mouseout", () => statusDiv.removeAttribute("style"));
+
+const h2party = document.querySelector('h2');
+h2party.addEventListener("click", () => {
+  const randomizer = Math.floor(Math.random() * 360);
+  h2party.style.color = `hsl(${randomizer}, 100%, 50%)`;
+});
 
 const statusHeader = document.createElement('h3');
 
-const renderGames = (games) => {
-    const tableBody = document.querySelector("#my-games-table-body")
-    games.forEach((game) => {
-        const tableRow = document.createElement("tr")
-    
-        addTableCell({ tableRow, value: game.name });
-        addTableCell({ tableRow, value: game.type });
-        addTableCell({ tableRow, value: game.rating });
-    
-        tableBody.appendChild(tableRow)
-      });
-    };
 
+const renderGames = (games) => {
+    const tableBody = document.querySelector("#my-games-table-body");
+
+    
+    games.forEach((game) => {
+      const tableRow = document.createElement("tr");
+      
+      addTableCell({ tableRow, value: game.name });
+      addTableCell({ tableRow, value: game.type });
+      addTableCell({ tableRow, value: game.rating });
+      
+      tableRow.addEventListener("click", () => {
+        clearStatus();
+        const statusHeader = document.createElement('h3');
+        statusHeader.textContent = 'Status';
+        document.getElementById("status").appendChild(statusHeader);
+        addStatus(toString(game));
+      });
+      
+      tableBody.appendChild(tableRow);
+    });
+  };
 statusHeader.textContent = 'Status';
 
 statusDiv.appendChild(statusHeader);
