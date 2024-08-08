2629240ed0f8bede57c989dbf18b46f00b84de84
Bram Verrando <dhapacifist@gmail.com>
2024-08-08 05:00:12 +0200

    working on form

---
+++ js/add-games.js
@@ -0,0 +1,26 @@
+const addGame = () => {
+    const name = document.querySelector("#name").value;
+    const type = document.querySelector("#type").value;
+    const rating = parseInt(document.querySelector("#rating").value);
+
+    const game = {
+        name, type, rating 
+    };
+
+    const response = fetch("http://localhost:3000/games", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(game)
+    });
+
+    addStatus(`Dit spel werd toegevoegd: ${game.name}`);
+};
+
+document.querySelector("#submit")
+.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addGame();
+});
