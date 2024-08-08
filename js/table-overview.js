65141838e5b312a6cd4150890c34d04c46fedfc7
Bram Verrando <dhapacifist@gmail.com>
2024-08-08 23:32:41 +0200

    cleaned code, removed redundancy

--- js/table-overview.js
+++ js/table-overview.js
@@ -4,78 +4,30 @@ let showFavourites = false;
 const fetchGames = async () => {
     const response = await fetch("http://localhost:3000/games");
     const result = await response.json();
-    games.length = 0;
-    games.push(...result);
+    games = result;
 };
 
 const searchByFetch = async (chars) => {
     const response = await fetch(`http://localhost:3000/games?query=${chars}`);
-    const result = await response.json();
-    return result;
+    return await response.json();
 };
 
-const toString = (game) => {
-    return `Name: ${game.name} - Type: ${game.type} - Rating: ${game.rating} - Favourite: ${game.isFavourite}`;
-};
+const toString = (game) => `Name: ${game.name} - Type: ${game.type} - Rating: ${game.rating} - Favourite: ${game.isFavourite}`;
 
 const statusDiv = document.createElement('div');
 statusDiv.id = 'status';
-
-statusDiv.addEventListener("mouseover", () => statusDiv.setAttribute("style", "background-color: green;"));
-statusDiv.addEventListener("mouseout", () => statusDiv.removeAttribute("style"));
+statusDiv.addEventListener("mouseover", () => statusDiv.style.backgroundColor = "green");
+statusDiv.addEventListener("mouseout", () => statusDiv.style.backgroundColor = "");
+document.querySelector('main').appendChild(statusDiv);
 
 const h2party = document.querySelector('h2');
 h2party.addEventListener("click", () => {
-    const randomizer = Math.floor(Math.random() * 360);
-    h2party.style.color = `hsl(${randomizer}, 100%, 50%)`;
+    h2party.style.color = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
 });
-
-const statusHeader = document.createElement('h3');
-
-const renderGames = (games) => {
-    const searchField = document.querySelector("#search-field");
-    const searchQuery = searchField.value;
-    const tableBody = document.querySelector("#my-games-table-body");
-    clearTableRows({ tableBody: 'my-games-table-body' });
-
-    if (games.length === 0) {
-        hideTable();
-        const statusMessage = searchQuery ? `No games available in the library with characters '${searchQuery}'.` : 'No games available in the library.';
-        document.querySelector("#status").textContent = statusMessage;
-        return;
-    }
-    clearStatus()
-    displayTable()
-
-    games.forEach((game) => {
-        const tableRow = document.createElement("tr");
-        addTableCell({ tableRow, value: game.name });
-        addTableCell({ tableRow, value: game.type });
-        addTableCell({ tableRow, value: game.rating });
-        tableRow.appendChild(createDeleteButton(game));
-
-        tableRow.addEventListener("click", () => {
-            clearStatus();
-            const statusHeader = document.createElement('h3');
-            statusHeader.textContent = 'Status';
-            document.getElementById("status").appendChild(statusHeader);
-            addStatus(toString(game));
-        });
-
-        tableRow.addEventListener("dblclick", () => {
-            toggleFavourite(game);
-
-            clearStatus();
-            addStatus(`The game with the name ${game.name} is now (not) my favourite.`);
-        });
-
-        tableBody.appendChild(tableRow);
-    });
-};
 
 const fetchAndRenderGames = async () => {
     await fetchGames();
-    
+
     const ratingField = document.querySelector("#rating-field");
     const searchField = document.querySelector("#search-field");
 
@@ -88,11 +40,9 @@ const fetchAndRenderGames = async () => {
         filteredGames = await searchByFetch(searchQuery);
         document.querySelector('#table-caption').textContent = `Games with name containing '${searchQuery}'`;
     } else if (showFavourites) {
-
         filteredGames = filteredGames.filter(game => game.isFavourite);
         document.querySelector('#table-caption').textContent = `Favourite Games`;
     } else {
-
         document.querySelector('#table-caption').textContent = `All games`;
     }
 
@@ -100,16 +50,37 @@ const fetchAndRenderGames = async () => {
         filteredGames = filteredGames.filter(game => game.rating > rating);
     }
 
-    renderGames(filteredGames);
+    const tableBody = document.querySelector("#my-games-table-body");
+    tableBody.innerHTML = "";
+
+    if (filteredGames.length === 0) {
+        hideTable();
+        document.querySelector("#status").textContent = searchQuery ? `No games available in the library with characters '${searchQuery}'.` : 'No games available in the library.';
+        return;
+    }
+    displayTable();
+
+    filteredGames.forEach((game) => {
+        const tableRow = document.createElement("tr");
+        addTableCell({ tableRow, value: game.name });
+        addTableCell({ tableRow, value: game.type });
+        addTableCell({ tableRow, value: game.rating });
+        tableRow.appendChild(createDeleteButton(game));
+
+        tableRow.addEventListener("click", () => {
+            clearStatus();
+            addStatus(toString(game));
+        });
+
+        tableRow.addEventListener("dblclick", () => {
+            toggleFavourite(game);
+            clearStatus();
+            addStatus(`The game with the name ${game.name} is now (not) my favourite.`);
+        });
+
+        tableBody.appendChild(tableRow);
+    });
 };
-
-statusHeader.textContent = 'Status';
-
-statusDiv.appendChild(statusHeader);
-
-document.querySelector('main').appendChild(statusDiv);
-
-
 
 document.querySelector("#show-favourites").addEventListener("click", () => {
     showFavourites = true;
@@ -117,16 +88,13 @@ document.querySelector("#show-favourites").addEventListener("click", () => {
 });
 
 document.querySelector("#show-all").addEventListener("click", () => {
-    showFavourites = false; 
+    showFavourites = false;
     fetchAndRenderGames();
 });
-
 
 document.querySelector("#rating-field").addEventListener("input", () => {
     fetchAndRenderGames();
 });
-
-
 
 const toggleFavourite = async (game) => {
     await fetch(`http://localhost:3000/games/${game.id}/Favourite`, {
@@ -136,7 +104,6 @@ const toggleFavourite = async (game) => {
             "Content-Type": "application/json"
         }
     });
-
     fetchAndRenderGames();
 }
 
@@ -144,7 +111,6 @@ const createDeleteButton = (game) => {
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     deleteButton.addEventListener("click", () => deleteGame(game));
-
     const deleteCell = document.createElement("td");
     deleteCell.appendChild(deleteButton);
     return deleteCell;
@@ -163,18 +129,13 @@ const deleteGame = async (game) => {
     fetchAndRenderGames();
 };
 
-const hideTable = () => {
-    const table = document.querySelector("table");
-    if (table) {
-        table.classList.add("hidden");
-    }
-};
+
+const hideTable = () => document.querySelector("table").classList.add("hidden");
 
 const displayTable = () => {
-    const table = document.querySelector("table");
-    if (table) {
-        table.classList.remove("hidden");
-    }
+    document.querySelector("table").classList.remove("hidden")
+    clearStatus()
 };
+
 fetchAndRenderGames();
 setInterval(fetchAndRenderGames, 1000);
